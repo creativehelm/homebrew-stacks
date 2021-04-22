3eb89a22af88d28128e63b411ec8db1220c82d41
creativehelm <creativehelm@gmail.com>
2021-04-21 18:47:10 -0700

    Update gallery-dl.rb

--- Formula/gallery-dl.rb
+++ Formula/gallery-dl.rb
@@ -1,11 +1,24 @@
 class GalleryDl < Formula
   include Language::Python::Virtualenv
 
-  desc "Command-line program to download image-galleries and -collections from several image hosting sites"
+  desc "Command-line program to download image galleries and collections from several image hosting sites"
   homepage "https://github.com/mikf/gallery-dl"
-  url "https://github.com/mikf/gallery-dl/archive/v1.13.2.tar.gz"
-  sha256 "0c4e4bcd917ad84e7e852862f01dcbe0e672f113ab03241b24c9c677ca2a77a5"
+  url "https://github.com/mikf/gallery-dl/archive/v1.16.1.tar.gz"
+  sha256 "ab3ba8e84366e54944673a2a1cfeb05eea16da2f27ff0bb6067fe055e01db283"
   head "https://github.com/mikf/gallery-dl.git"
+  license "GPL-2.0"
+
+  livecheck do
+    url "https://github.com/mikf/gallery-dl/releases/latest"
+    regex(%r{href=.*?/tag/v?(\d+(?:\.\d+)+)["' >]}i)
+  end
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9efe9b88dd4999412396bde9543b65152fcf5e75b25307b39826c8b3149369ce" => :catalina
+    sha256 "a31510083974a5feca324255b43d9f7b2261fb66776a1852e15bc646144dd3b6" => :mojave
+    sha256 "44afe4d34e4f6813139128c54b6afe740d03a9753acf8f5e164732e3933e2546" => :high_sierra
+  end
 
   depends_on "python" => :recommended
 
@@ -35,5 +48,10 @@ class GalleryDl < Formula
                               "--ignore-installed", buildpath
     system libexec/"bin/pip", "uninstall", "-y", "gallery-dl"
     venv.pip_install_and_link buildpath
+  end
+
+  test do
+    # Test danbooru
+    system "#{bin}/gallery-dl", "--simulate", "https://danbooru.donmai.us/posts?tags=bonocho"
   end
 end
